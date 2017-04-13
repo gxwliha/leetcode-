b0ae51172ab44846be238eb6e495537d178c8ef4
unknown <>
2017-04-13 16:42:28 +0800

    添加题目

--- SingleNumber.java
+++ SingleNumber.java
@@ -3,6 +3,18 @@ package com.example.gxw.myapplication;
 /**
  * Created by GXW on 2017/4/13.
  */
+ /*题目：
+Given an array of integers, every element appears twice except for one. Find that single one.
+
+Note:
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+
+思路：
+直接排序或对比，时间复杂度一定在nlogn以上。利用Hash表或者Bitmap可以满足时间复杂度但是空间复杂度不能满足。
+O(n)时间类能采用的办法只有异或。
+ 
+ */
 
 public class SingleNumber {
     public static void main(String args[]) {
